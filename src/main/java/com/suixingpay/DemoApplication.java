dc8d7466086c897ff5fdd7dba65d19ab18a46223
chi_yj <chi_yj@suixingpay.com>
2018-10-24 15:43:58 +0800

    first commit

---
+++ src/main/java/com/suixingpay/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.suixingpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource(value = {"classpath*:core-context.xml"})
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
