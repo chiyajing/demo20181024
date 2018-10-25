f9c701353936301b07a78b466563d79fecf910e2
chi_yj <chi_yj@suixingpay.com>
2018-10-25 15:35:25 +0800

    fix

--- src/main/java/com/suixingpay/TestController.java
+++ src/main/java/com/suixingpay/TestController.java
@@ -14,7 +14,7 @@ public class TestController {
     //list sadfdasfdasfsad
     @RequestMapping(value = "/list", method = {RequestMethod.GET})
     public String list() {
- //TODOddd fix
+ //TODOddd fix sfafdsa
      return "test list";
 
     }
